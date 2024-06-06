55b49e7482e05f7d2593f42ccb5f4722ea178d57
Abhishek Kumar Ray <abhishekray7261@gmail.com>
2024-06-06 21:05:02 +0530

    react-firebase

---
+++ src/firebase/Firebase.jsx
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmsJN0o8lx449STXFsSMxhdnTCbugMzPc",
+  authDomain: "auth-2b38f.firebaseapp.com",
+  projectId: "auth-2b38f",
+  storageBucket: "auth-2b38f.appspot.com",
+  messagingSenderId: "1011260223582",
+  appId: "1:1011260223582:web:a0c50b23a374852cfe84cf"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const fireDb = getFirestore(app)
